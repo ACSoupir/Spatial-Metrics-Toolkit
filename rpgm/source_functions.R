12f03db4ba7791e3b719433b2b2e6723d333cbc8
ACSoupir <alex.soupir@jacks.sdstate.edu>
2024-12-17 08:28:21 -0500

    updated sourcing, adding slurm

---
+++ rpgm/source_functions.R
@@ -0,0 +1,12 @@
+
+# sourcing functions ------------------------------------------------------
+
+#processing functions
+source("rpgm/create_folders.R")
+source("rpgm/kest.R")
+source("rpgm/gest.R")
+source("rpgm/dbscan.R")
+source("rpgm/xy_point.R")
+source("rpgm/Summarise_function.R")
+source("rpgm/validations.R")
+source("rpgm/0.4.cell_distribution_across_samples_boxplot.R")
